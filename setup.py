f7f2b99d50c1de39f94090ac3279930140a14980
Prakash Autade <prakash.autade@gmail.com>
2018-06-17 00:15:39 +0530

    Adding dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/prakashautade/twitterstream",
     packages=setuptools.find_packages(),
+    install_requires=[ 'tweepy', ]
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
